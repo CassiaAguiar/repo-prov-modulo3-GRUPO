8158bc4b560cbec0d5f3ada48160d2151abe91c8
CassiaAguiar <114167520+CassiaAguiar@users.noreply.github.com>
2022-12-13 12:05:23 -0300

    Add files via upload

---
+++ Aula 3.9/meu-app/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import BaseTable from './components/BaseTable'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <BaseTable/>
+      
+    </div>
+  )
+}
+
+export default App
---
+++ Aula 3.9/meu-app/src/components/BaseTable.jsx
@@ -0,0 +1,78 @@
+import React from "react"
+import  Table  from "react-bootstrap/Table"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios"
+import { useEffect, useState } from 'react'
+import Button from 'react-bootstrap/Button';
+
+export default function BaseTable()
+{
+    const [rows, setRows] = useState([])
+    const [name, setName] = useState([])
+    const [email, setEmail] = useState([])
+    const [idade, setIdade] = useState([])
+
+    useEffect(() =>{
+        axios.get('http://localhost:3002/alunos')
+        .then((Response) => setRows(Response.data))
+        .then((error) => console.log(error))
+    },[])
+    //----------Inserindo valores----------//
+    const inputNome = (e) =>{
+        setName(e.target.value)
+    }
+    const inputEmail = (e) =>{
+        setEmail(e.target.value)
+    }
+    const inputIdade = (e) =>{
+        setIdade(e.target.value)
+    }
+    function postName()
+    {
+        axios.post('',{nome: name, email: email, idade: idade } )
+        .then(axios.get(''))
+        then((Response) => setRows(Response.data))
+        .then((error) => console.log(error))
+
+    }
+
+
+    return(
+        <Table striped bordered hover>
+        <thead>
+            <tr>
+                <header>
+                    <label>Nome</label>
+                    <input onChange={ (e) => inputNome(e) } /> {' '}
+                    <label>E-mail</label>
+                    <input onChange={ (e) => inputEmail(e) } /> {' '}
+                    <label>idade</label>
+                    <input onChange={ (e) => inputIdade(e) } /> {' '}
+                    <Button variant="primary" onClick={() => postName() }>inserir</Button>{' '}
+                </header>
+            </tr>
+          <tr>
+            <th>ID</th>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Idade</th>
+            <th>Editar</th>
+            <th>Excluir</th>
+          </tr>
+        </thead>
+        <tbody>
+            {rows.map((row) => (
+            <tr key={row.id}>
+             <td>{row.id}</td>
+             <td>{row.nome}</td>
+             <td>{row.email}</td>
+             <td>{row.idade}</td>
+             <td><Button variant="primary">Editar</Button>{' '}</td>
+             <td><Button variant="danger">Excluir</Button>{' '}</td>
+           </tr>
+            ))}
+         
+        </tbody>
+      </Table>
+    ) 
+}
